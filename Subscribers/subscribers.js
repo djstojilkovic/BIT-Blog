822730aa9285ab19235c7b651a4524c57d3e5ce8
djstojilkovic <djstojilkovic@gmail.com>
2024-11-20 11:32:05 +0100

    BIT Blog Final

--- Subscribers/subscribers.js
+++ Subscribers/subscribers.js
@@ -1,6 +1,9 @@
 const SUBS_URL =
   "https://673c4f8796b8dcd5f3f963e6.mockapi.io/api/v1/subscriptions";
 window.addEventListener("load", getSubs);
+const divSub = document.querySelector(".divSub");
+const divUnsub = document.querySelector(".divUnsub");
+const subsCont = document.querySelector(".subsContainer");
 
 function getSubs() {
   fetch(SUBS_URL)
@@ -10,22 +13,23 @@ function getSubs() {
     });
 }
 function showSubs(data) {
+  divSub.innerHTML = "";
+  divUnsub.innerHTML = "";
   data.forEach((e) => {
-    console.log(e);
     const div = document.createElement("div");
     if (e.isSubscribed === true) {
       div.className = "subsDiv";
       div.innerHTML = `${e.name}, ${e.email} - `;
-      document.querySelector(".divSub").append(div);
+      divSub.append(div);
     } else {
       div.className = "unsubsDiv";
       div.innerHTML = `${e.name}, ${e.email} +`;
-      document.querySelector(".divUnsub").append(div);
+      divUnsub.append(div);
     }
+
     div.style.cursor = "pointer";
     // put metoda za prebacivanje iz subs-a u unsubs-e
     div.addEventListener("click", () => {
-      console.log("dasdas");
       fetch(
         `https://673c4f8796b8dcd5f3f963e6.mockapi.io/api/v1/subscriptions/${e.id}`,
         {
@@ -39,10 +43,7 @@ function showSubs(data) {
         }
       )
         .then((response) => response.json())
-        .then((json) => console.log(json))
-        .then(() => {
-          getSubs();
-        });
+        .then((json) => getSubs());
     });
   });
 }
